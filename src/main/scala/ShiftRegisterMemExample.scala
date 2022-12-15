7c70d40e3a7fdd033aad95781b1f60874bbefe56
Vladimir Milovanović <barun@eecs.berkeley.edu>
2022-12-15 13:55:18 +0100

    Change shift register to be with and without annotations

--- src/main/scala/ShiftRegisterMemExample.scala
+++ src/main/scala/ShiftRegisterMemExample.scala
@@ -1,10 +1,10 @@
-package backendTest
+package shiftregmem
 
 import chisel3._
 import chisel3.util._
 
 import craft.ShiftRegisterMem
-
+import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
 
 class ShiftRegisterIO[T <: Data](gen: T, n: Int) extends Bundle {
   require (n >= 0, "Shift register must have non-negative shift")
@@ -17,13 +17,19 @@ class ShiftRegisterIO[T <: Data](gen: T, n: Int) extends Bundle {
   override def cloneType: this.type = (new ShiftRegisterIO(gen, n)).asInstanceOf[this.type]
 }
 
-class ShiftRegisterMemExample[T <: Data](gen: T, n: Int) extends Module {
+class ShiftRegisterExample[T <: Data](gen: T, n: Int, isMem: Boolean, isSram: Option[Boolean]) extends Module {
   val io = IO(new ShiftRegisterIO(gen, n))
+
   val logn = log2Ceil(n)
   val cnt = RegInit(0.U(logn.W))
+  val dataWidth = gen.getWidth
 
-  val shiftMem = ShiftRegisterMem(io.in, n, io.en, name = "simple_shift_register")
-  //val shiftMem = ShiftRegister(io.in, n, io.en)
+  val memFlag = if (isMem) "mem" else "reg"
+  val sramFlag = if (isSram.getOrElse(false)) "_sram" else ""
+
+  val shiftMem = if (memFlag == "mem") ShiftRegisterMem(io.in, n, io.en, name = "simple_shift_reg" + "_width_" + dataWidth.toString + "_depth_" + n.toString + "_" + memFlag + sramFlag) else ShiftRegister(io.in, n, io.en)
+  override def desiredName = "shift_reg" + "_width_" + dataWidth.toString + "_depth_" + n.toString + "_" + memFlag + sramFlag
+
   io.out := shiftMem
  
   when (io.en === true.B) {
@@ -37,7 +43,37 @@ class ShiftRegisterMemExample[T <: Data](gen: T, n: Int) extends Module {
   io.valid_out := initialInDone && io.en
 }
 
-object ShiftRegisterMemApp extends App
+object ShiftRegisterApp extends App
 {
-  chisel3.Driver.execute(args,()=>new ShiftRegisterMemExample(UInt(4.W), 10))
+  implicit def int2bool(b: Int) = if (b == 1) true else false
+  if (args.length < 4) {
+    println("This application requires at least 4 arguments")
+  }
+  val buildDirName = args(0).toString
+  val width = args(1).toInt
+  val depth = args(2).toInt
+  val isMem = int2bool(args(3).toInt)
+  val isSram : Option[Boolean] = if (args.length == 4) None else Some(true)
+
+  if (isMem & isSram.getOrElse(false)) {
+    val arguments = Array(
+      "--target-dir", buildDirName,
+      "-X", "verilog",
+      "--repl-seq-mem", "-c:ShiftRegisterMemExample:-o:mem.conf",
+      "--log-level", "info"
+    )
+  // generate black boxes for memories
+    (new ChiselStage).execute(
+      arguments,
+      Seq(ChiselGeneratorAnnotation(() => new ShiftRegisterExample(SInt(width.W), depth, isMem, isSram))))
+  }
+  else {
+    val arguments = Array(
+      "--target-dir", buildDirName,
+      "-X", "verilog"
+    )
+    (new chisel3.stage.ChiselStage).execute(
+      arguments,
+      Seq(ChiselGeneratorAnnotation(() => new ShiftRegisterExample(SInt(width.W), depth, isMem, isSram))))
+  }
 }
